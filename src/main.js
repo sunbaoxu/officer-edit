5111ac62a4f5b118a0acada7566347672d7bf81d
sunbaoxu <1194063234@qq.com>
2019-06-28 16:46:55 +0800

    引入视频、富文本

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store/'
 import ElementUI from 'element-ui'
-
+import  VueQuillEditor from 'vue-quill-editor'
+import VideoPlayer from 'vue-video-player';
+// require styles 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css';
 
 require('@/assets/css/reset.scss');
 import 'element-ui/lib/theme-chalk/index.css'
@@ -12,6 +17,8 @@ Vue.config.productionTip = false
 
 
 Vue.use(ElementUI);
+Vue.use(VueQuillEditor)
+Vue.use(VideoPlayer)
 
 new Vue({
   router,
