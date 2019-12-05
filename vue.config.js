e05f67e653baeb1f239a6c96b49319c1e15e3704
sunbaoxu <1194063234@qq.com>
2019-12-05 14:32:17 +0800

    路径1

--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,7 @@ module.exports = {
     publicPath:'./',
     // 构建好的文件输出到哪里
     outputDir: "officer-dist", 
+    assetsDir :'static',
     // where to put static assets (js/css/img/font/...) // 是否在保存时使用‘eslint-loader’进行检查 // 有效值: true | false | 'error' // 当设置为‘error’时，检查出的错误会触发编译失败
     lintOnSave: true, // 使用带有浏览器内编译器的完整构建版本 // https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
     runtimeCompiler: false, // babel-loader默认会跳过`node_modules`依赖. // 通过这个选项可以显示转译一个依赖
